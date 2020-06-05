057b5099ce6105c197668354a128490cc33cc844
esmoreido <moreido@mail.ru>
2020-06-05 15:03:34 +0300

    q variables excluded

--- dopler.py
+++ dopler.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import time
 import datetime
 from datetime import date, timedelta
 import time
@@ -17,62 +18,6 @@ import seaborn as sns
 import os
 import glob
 from os.path import splitext
-
-# дописать графику
-def graph_plot(data):
-    # пересчет на сетку
-    # x = np.linspace(bsf['dist'].min(), bsf['dist'].max(), len(bsf['dist'].unique()))
-    # y = np.linspace(bsf['hb'].min(), bsf['hb'].max(), len(bsf['hb'].unique()))
-    x = np.linspace(bsf['dist'].min(), bsf['dist'].max(), 100)
-    y = np.linspace(bsf['hb'].min(), bsf['hb'].max(), 100)
-    X, Y = np.meshgrid(x, y)
-    Z = griddata((bsf['dist'], bsf['hb']), bsf['value'], (X, Y), method='linear')
-
-    fig = plt.figure(figsize=[16, 8])
-
-    # 2D рисунок
-    ax = fig.add_subplot(111)
-    ff = ax.contourf(X, Y, Z, 8, cmap=cm.coolwarm)
-    ax.plot(bsf['dist'], bsf['h'], color='Black', linestyle='-', linewidth=2)
-    contour_labels = plt.contour(X, Y, Z, 8, colors='black', linewidth=.5)
-    ax.clabel(contour_labels, inline=1, fontsize=10)
-    cbar = fig.colorbar(ff, shrink=0.5, aspect=5)
-    ax.set_xlabel('Расстояние')
-    ax.set_ylabel('Глубина')
-    cbar.set_label('Интенсивность рассеяния, дБ')
-    ax.invert_yaxis()
-    plt.savefig(file_out + '.png', dpi=150, facecolor='w', edgecolor='w',
-                orientation='landscape', )
-    plt.show()
-
-    # переделать графику через Tricontour Smooth Delaunay
-    # https://matplotlib.org/3.1.1/gallery/images_contours_and_fields/tricontour_smooth_delaunay.html#sphx-glr-gallery-images-contours-and-fields-tricontour-smooth-delaunay-py
-
-    # 3D рисунок
-    # fig = plt.figure()
-    # ax = fig.gca(projection='3d')
-    # surf = ax.plot_surface(X, Y, Z, rstride = 10, cstride = 1, cmap=cm.coolwarm,
-    #                        linewidth=1, antialiased=False)
-    # fig.colorbar(surf, shrink=0.5, aspect=5)
-    # ax.invert_zaxis()
-    # plt.savefig(file_out + '_3d.png', dpi=150, facecolor='w', edgecolor='w',
-    #             orientation='landscape', )
-    # plt.show()
-
-    # триангуляция
-    # fig = plt.figure()
-    # ax = Axes3D(fig)
-    # # surf = ax.plot_trisurf(bsf.iloc[:,0], bsf.iloc[:,1], bsf.iloc[:,2], cmap=cm.jet, linewidth=0.1)
-    # surf = ax.plot_trisurf(X,Y,Z, cmap=cm.jet, linewidth=0.1)
-    # fig.colorbar(surf, shrink=0.5, aspect=5)
-    # ax.set_xlabel('Расстояние')
-    # ax.set_ylabel('Глубина')
-    # ax.set_zlabel('Скорость')
-    # ax.invert_zaxis()
-    # # ax.view_init(azim=200, elev=30)
-    # ax.text2D(0.05, 0.95, 'Az {}, Z-elev {}'.format(ax.azim, ax.elev), transform=ax.transAxes)
-    # plt.show()
-
 
 def get_chunk(file, n):
     return [x.strip() for x in islice(file, n)]
@@ -92,8 +37,11 @@ def head_proc(lines):
     # широта и долгота, если есть
     res['lat'] = float(lines[3].split()[0]) if float(lines[3].split()[0]) != 30000. else 'NA'
     res['lon'] = float(lines[3].split()[1]) if float(lines[3].split()[1]) != 30000. else 'NA'
+    # вычисленное значение расхода в поверхностном слое, нарастающей суммой от начала профиля
     res['top_q'] =  round(float(lines[4].split()[1]), 3)
+    # вычисленное значение расхода в придонном слое
     res['bot_q'] =  round(float(lines[4].split()[2]), 3)
+    # количество ячеек в ансамбле
     res['nbins'] = int(lines[5].split()[0])
 
     # print(res)
@@ -104,7 +52,7 @@ def ens_proc(ens, ensnum, ensdist, ensh, enslat, enslon, topq, botq):
     res = {}
     df = pd.DataFrame([x.split() for x in ens], columns= ['hb', 'v', 'd', 'v1', 'v2', 'v3', 'v4',
                                                           'bs1', 'bs2', 'bs3', 'bs4', 'percgood', 'q'])
-    rmcols = ['v1', 'v2', 'v3', 'v4', 'percgood'] # ненужные колонки
+    rmcols = ['v1', 'v2', 'v3', 'v4', 'percgood', 'q'] # ненужные колонки
     df.drop(rmcols, inplace = True, axis=1) # удаляем ненужные
     df = df.replace(dict.fromkeys(['-32768', '2147483647', '255'], np.nan)) # замена отсутствующих данных
     df = df.dropna() # удаляем отсутствующие
@@ -115,8 +63,8 @@ def ens_proc(ens, ensnum, ensdist, ensh, enslat, enslon, topq, botq):
     df['h'] = ensh
     df['lat'] = enslat
     df['lon'] = enslon
-    df['top_q'] = topq
-    df['bot_q'] = botq
+    # df['top_q'] = topq
+    # df['bot_q'] = botq
     df['v'] = df['v'].values * 0.01 # переводим скрость в метры в секунду
     df = df.round(3)
     res = pd.melt(df, id_vars=['ens', 'dist', 'lon', 'lat', 'hb', 'h']) # переформатируем в длинный формат
@@ -159,21 +107,9 @@ def file_proc(path_in, path_out):
 
 # главный модуль
 if __name__ == "__main__":
-    # path = r'I:\Селенга2016\Доплер СТВОРЫ ВСЕ ВМЕСТЕ'
-    # proc_list = []
-    # for root, subfolders, files in os.walk(path):
-    #     for f in files:
-    #         if splitext(f)[1].lower() == ".txt":
-    #             # print(root + "\\" + f)
-    #             proc_list.append(root + "\\" + f)
-
-    # file_proc(proc_list[0])
-    # for f in proc_list:
-    #     print('Processing: ' + f)
-    # os.chdir(r"c:\temp\ob")
-    # ff = glob.glob("*.txt")
-    # for f in ff:
-    #     file_in = f
-    #     file_out = f.replace("txt", "out")
-    #     file_proc(file_in, file_out)
-    file_proc(r'c:\temp\doppler\2019-06-08\1_0\1_0_000_ASC.TXT', r'c:\temp\doppler\2019-06-08\1_0\1_0_000_ASC.out')
+    ff = glob.glob("*.txt")
+    print("Detected ASCII *.txt files: \n", "\n".join(ff))
+    for f in ff:
+        file_in = f
+        file_out = f.replace("txt", "csv")
+        file_proc(file_in, file_out)
